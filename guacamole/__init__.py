f130fc50f3390209843657066ea9feca62deda2f
s0er3n <soeren.michaels@gmail.com>
2019-12-15 21:00:28 +0100

    messaging_system

--- guacamole/__init__.py
+++ guacamole/__init__.py
@@ -10,19 +10,17 @@ def create_app(test_config=None):
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "guacamole.sqlite"),
     )
 
 
 
     # apply the blueprints to the app
-    from guacamole import auth, marketplace, me
+    from guacamole import auth, marketplace, me, messaging_system
 
+    app.register_blueprint(messaging_system.bp)
     app.register_blueprint(auth.bp)
     app.register_blueprint(marketplace.bp)
     app.register_blueprint(me.bp)
-
     #make url_for('index') == url_for('marketplace.index')
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
