16151d18167f3558bbf6b66416153613035e2868
Sören <soeren.michaels@gmail.com>
2020-02-03 19:21:48 +0100

    random stuff dont ask pls

--- guacamole/messaging_system.py
+++ guacamole/messaging_system.py
@@ -5,6 +5,7 @@ from guacamole import socketio
 from guacamole.auth import login_required
 from flask import jsonify
 from flask_socketio import join_room
+
 bp = Blueprint("messaging_system", __name__, url_prefix="/messaging_system")
 
 
@@ -12,19 +13,15 @@ def send_message(message, room):
     """pushes message to firebase as someone who bid"""
     from datetime import datetime
     import calendar
+
     d = datetime.utcnow()
     unixtime = calendar.timegm(d.utctimetuple())
     firebase.db.child("messaging_system").child(room).push(
-        {   
-            "message": message,
-            "sender": session["user_id"],
-            "timestamp": unixtime,
-        }
+        {"message": message, "sender": session["user_id"], "timestamp": unixtime,}
     )
 
 
-
-@bp.route("/send/<room>")
+@bp.route("/chat/<room>")
 def sendroom(room):
     from datetime import datetime
 
@@ -36,19 +33,25 @@ def sendroom(room):
     except:
         pass
     print(listofmessagesasstring)
-    return render_template("messaging_system/chat.html", utcFromTimestamp=datetime.utcfromtimestamp, listofmessagesasstring= listofmessagesasstring, room = room)
+    return render_template(
+        "messaging_system/chat.html",
+        utcFromTimestamp=datetime.utcfromtimestamp,
+        listofmessagesasstring=listofmessagesasstring,
+        room=room,
+    )
+
 
 @socketio.on("join")
 def join(room):
     join_room(room)
 
 
-
 @socketio.on("message")
 def handle_message(message):
     print("received message: " + message.get("message"))
-    send_message(message.get("message"), message.get("room"))
-    socketio.send(message.get("message"))
-    
-    
+    try:
+        send_message(message.get("message"), message.get("room"))
+        socketio.send(message.get("message"))
+    except:
+        print("DU BIST NICHT EINGELOGGT")
 
